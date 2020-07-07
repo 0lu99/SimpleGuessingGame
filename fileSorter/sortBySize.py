96547d4f7f8e9a6c599f2767c9bfb6a8ac7664a9
Olu <olujs1999@gmail.com>
2020-07-07 22:40:00 +0100

    removed 'f'

--- fileSorter/sortBySize.py
+++ fileSorter/sortBySize.py
@@ -7,4 +7,4 @@ files = os.listdir(os.getcwd())
 by default, it will print from smallest to largest, so order has been reversed
 '''
 files.sort(key=lambda f: os.stat(f).st_size, reverse=True)
-print(files + f)
+print(files)
