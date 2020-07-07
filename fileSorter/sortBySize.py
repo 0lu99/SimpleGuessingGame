abbb54a389bd4ea806fa17184437a112642a9aed
Olu <olujs1999@gmail.com>
2020-07-07 22:28:06 +0100

    sortBySize.py addition

---
+++ fileSorter/sortBySize.py
@@ -0,0 +1,10 @@
+import os
+
+' List the files in the current working directory'
+files = os.listdir(os.getcwd())
+
+'''
+by default, it will print from smallest to largest, so order has been reversed
+'''
+files.sort(key=lambda f: os.stat(f).st_size, reverse=True)
+print(files + f)
