2ea57d5100bb2b76aee9180c4f51817df0ef6f48
Olu <olujs1999@gmail.com>
2020-08-13 22:22:17 +0100

    removeDuplicatedList

---
+++ removeDuplicateList/removeDuplicateList.py
@@ -0,0 +1,17 @@
+def remove_dupe1(x):
+    y = []
+    for i in x:
+        if i not in y:
+            y.append(i)
+    return y
+
+
+# using sets
+def remove_dupe2(x):
+    return list(set(x))
+
+
+a = [1, 2, 3, 4, 3, 2, 1]
+print(a)
+print(remove_dupe1(a))
+print(remove_dupe2(a))
