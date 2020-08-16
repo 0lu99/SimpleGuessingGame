c39f76dafba3e80251904f0707a7021765c2cde9
Olu <olujs1999@gmail.com>
2020-08-16 13:11:31 +0100

    scrapeWebpage.py

---
+++ scrapeWebpage/scrapeWebpage.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.nytimes.com/'
+r = requests.get(url)
+soup = BeautifulSoup(r.text, "html.parser")
+
+for story_heading in  soup.find_all('h2', {'class': 'css-1cmu9py e1voiwgp0'}):
+    print(story_heading.text)
+
